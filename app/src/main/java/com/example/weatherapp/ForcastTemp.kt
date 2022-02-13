a45dc25f1d2d7be5577d1802f84e78e4be0af87f
maxxyangg <61856860+maxxyangg@users.noreply.github.com>
2022-02-13 00:10:11 -0600

    Assignment #3 Fixes

--- app/src/main/java/com/example/weatherapp/ForcastTemp.kt
+++ app/src/main/java/com/example/weatherapp/ForcastTemp.kt
@@ -1,7 +1,3 @@
 package com.example.weatherapp
 
-data class ForcastTemp (val day : Float, val min : Float, val max : Float){
-
-
-
-}
+data class ForcastTemp (val day : Float, val min : Float, val max : Float);
