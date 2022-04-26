c6a0efdb241c832a7172df5c8e5ef761143c0161
maxxyangg <61856860+maxxyangg@users.noreply.github.com>
2022-04-26 16:24:06 -0500

    Assignment#8

--- app/src/main/java/com/example/weatherapp/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/SearchFragment.kt
@@ -1,45 +1,32 @@
 package com.example.weatherapp.fragments
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
-import android.content.IntentSender
+import android.app.*
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Geocoder
-import android.location.Location
-import android.location.LocationManager
-import android.os.Build
+
 import android.os.Bundle
 import android.os.Looper
+
 import android.text.Editable
-import android.text.Layout
 import android.text.TextWatcher
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
+
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+
+import androidx.core.content.ContextCompat.startForegroundService
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.findNavController
-import com.example.weatherapp.MainActivity
+
+import com.example.weatherapp.MyService
 import com.example.weatherapp.R
-import com.example.weatherapp.viewmodels.SearchViewModel
 import com.example.weatherapp.databinding.FragmentSearchBinding
-import com.google.android.gms.common.api.ResolvableApiException
+import com.example.weatherapp.viewmodels.SearchViewModel
 import com.google.android.gms.location.*
-import com.google.android.gms.tasks.Task
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.IOException
-import java.util.*
-import java.util.jar.Manifest
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class SearchFragment: Fragment(R.layout.fragment_search), ActivityCompat.OnRequestPermissionsResultCallback {
@@ -49,11 +36,11 @@ class SearchFragment: Fragment(R.layout.fragment_search), ActivityCompat.OnReque
     private val REQUEST_LOCATION_PERMISSION: Int = 123
 
 
-
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSearchBinding.bind(view)
+
+
 
         searchViewModel.enableButton.observe(viewLifecycleOwner){ enable ->
             binding.searchButton.isEnabled = enable
@@ -79,6 +66,8 @@ class SearchFragment: Fragment(R.layout.fragment_search), ActivityCompat.OnReque
         })
 
 
+
+
         binding.searchButton.setOnClickListener{
             try {
                 println("Button clicked")
@@ -97,8 +86,21 @@ class SearchFragment: Fragment(R.layout.fragment_search), ActivityCompat.OnReque
             requestLocationUpdates()
         }
 
+        binding.notificationButton.setOnClickListener {
+            if(isMyServiceRunning(MyService::class.java)){
+                requestLocationNotifications()
+                binding.notificationButton.setText(R.string.notificationsOnText)
+
+            }else{
+                requestLocationNotifications()
+                binding.notificationButton.setText(R.string.notificationsOffText)
+
+            }
+        }
+
 
     }
+
 
     private fun requestLocationPermission(){
         if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION)){
@@ -110,7 +112,6 @@ class SearchFragment: Fragment(R.layout.fragment_search), ActivityCompat.OnReque
                         arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
                         REQUEST_LOCATION_PERMISSION
                     )
-
                 }
                 .create()
                 .show()
@@ -129,20 +130,20 @@ class SearchFragment: Fragment(R.layout.fragment_search), ActivityCompat.OnReque
         ) {
             //  when permission is already granted
             println("You have permissions")
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener {
+                    requestNewLocation()
+                    Log.d("MainActivity", it.toString())
+                    searchViewModel.updateCoordinates(it.latitude,it.longitude)
+                    searchViewModel.loadLocationData()
+                    val currentConditions = searchViewModel.currentConditions?.value!!
+                    findNavController().navigate(SearchFragmentDirections.actionSearchFragmentToCurrentConditionsFragment(binding.zipcode.text.toString(),currentConditions,currentConditions.coord))
+                }
         } else {
             requestLocationPermission()
         }
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.requireActivity())
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener {
-                requestNewLocation()
-                Log.d("MainActivity", it.toString())
-                searchViewModel.updateCoordinates(it.latitude,it.longitude)
-                searchViewModel.loadLocationData()
-                val currentConditions = searchViewModel.currentConditions?.value!!
-                findNavController().navigate(SearchFragmentDirections.actionSearchFragmentToCurrentConditionsFragment(binding.zipcode.text.toString(),currentConditions,currentConditions.coord))
-            }
 
     }
 
@@ -160,13 +161,14 @@ class SearchFragment: Fragment(R.layout.fragment_search), ActivityCompat.OnReque
 
         val locationRequest = LocationRequest.create()
         locationRequest.interval = 0L
-        locationRequest.fastestInterval = 0L
+        locationRequest.fastestInterval =  0L
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.setInterval(60000 * 15)
         val locationProvider = LocationServices.getFusedLocationProviderClient(this.requireActivity())
         val locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
                 locationResult.locations.forEach {
-
+                    println(it.toString())
                 }
             }
         }
@@ -179,11 +181,52 @@ class SearchFragment: Fragment(R.layout.fragment_search), ActivityCompat.OnReque
     }
 
 
+    private fun requestLocationNotifications(){
+        if (ActivityCompat.checkSelfPermission(
+                this.requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            //  when permission is already granted
+            println("You have permissions")
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.requireActivity())
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener {
+                    requestNewLocation()
+                    Log.d("MainActivity", it.toString())
+                    searchViewModel.updateCoordinates(it.latitude,it.longitude)
+                    searchViewModel.loadLocationData()
+                    val currentConditions = searchViewModel.currentConditions?.value!!
+                    var serviceIntent = Intent(requireContext(), MyService::class.java)
+                    serviceIntent.putExtra("CityName", currentConditions.name)
+                    serviceIntent.putExtra("CurrentTemp", currentConditions.main.temp)
+                    startForegroundService(requireContext(), serviceIntent)
+                }
+
+
+
+        } else {
+            requestLocationPermission()
+        }
+
+    }
+
+    private fun isMyServiceRunning(myClass: Class<MyService>): Boolean {
+        val manager: ActivityManager = requireActivity().getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+        @Suppress("DEPRECATION")
+        for(service: ActivityManager.RunningServiceInfo in manager.getRunningServices(Int.MAX_VALUE)){
+            if (myClass.name.equals(service.service.className)){
+
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 
 }
-
-
 
 
 
